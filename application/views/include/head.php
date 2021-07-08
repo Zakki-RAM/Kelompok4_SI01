eac041464fbdfaf9fb67f54e66e84d51bf376eb2
Zakki-RAM <zakkialhafidz70@gmail.com>
2021-07-08 13:17:08 +0700

    Perubahan sidebar.php, navbar.php, head.php

--- application/views/include/head.php
+++ application/views/include/head.php
@@ -6,7 +6,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Website Rumah Sakit</title>
+    <title>Website_siwikode</title>
 
     <!-- Custom fonts for this template-->
     <link href="<?= base_url('asset/fontawesome-free/css/all.min.css') ?>" rel="stylesheet" type="text/css">
--- application/views/include/sidebar.php
+++ application/views/include/sidebar.php
@@ -1,12 +1,12 @@
   <!-- Sidebar -->
-  <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+  <ul class="navbar-nav bg-gradient-dark sidebar sidebar-dark accordion" id="accordionSidebar">
 
 <!-- Sidebar - Brand -->
 <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
     <div class="sidebar-brand-icon rotate-n-15">
         <i class="fas fa-laugh-wink"></i>
     </div>
-    <div class="sidebar-brand-text mx-3">Website Rumah Sakit</div>
+    <div class="sidebar-brand-text mx-3">Website SIWIKODE</div>
 </a>
 
 <!-- Divider -->
@@ -32,7 +32,7 @@
     <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
         aria-expanded="true" aria-controls="collapseTwo">
         <i class="fas fa-fw fa-cog"></i>
-        <span>Pasien</span>
+        <span>Wisata Rekreasi</span>
     </a>
     <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
         <div class="bg-white py-2 collapse-inner rounded">
